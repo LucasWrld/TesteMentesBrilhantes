318882141ddcbdb57a4011bc6f627ad73b4a9869
LucasWrld <97101788+LucasWrld@users.noreply.github.com>
2025-09-10 15:05:40 -0300

    Update exercise01-angular.ts

--- exercise01-angular.ts
+++ exercise01-angular.ts
@@ -6,23 +6,13 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item',
-  template: `
-    <ul>
-      <li *ngFor="let it of list">{{ it }}</li>
-    </ul>
-
-    <input [(ngModel)]="newItem" />
-    <button (click)="addItem()">Adicionar</button>
-  `
 })
 export class ItemComponent {
-  @Input() list: string[] = [];
-  newItem = '';
 
-  addItem() {
-    if (this.newItem) {
-      this.list = [...this.list, this.newItem];
-      this.newItem = '';
-    }
+  @Input() list: string[] = [];
+
+  add(item: string) {
+    this.list = [...this.list, item];
   }
+
 }
