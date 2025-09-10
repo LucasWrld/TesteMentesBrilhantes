a968234626d8552182373e42590aa4e9ddf1c3a7
LucasWrld <97101788+LucasWrld@users.noreply.github.com>
2025-09-10 15:20:02 -0300

    Update exercise05-angular.ts

--- exercise05-angular.ts
+++ exercise05-angular.ts
@@ -7,13 +7,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-item',
   template: `
-	<div *ngIf="status === 'error'">Erro!</div>
-	<div *ngIf="status === 'success'">Sucesso!</div>
-	<div *ngIf="status === 'warning'">Atenção!</div>
+    <div [ngSwitch]="status">
+      <div *ngSwitchCase="'error'">Erro!</div>
+      <div *ngSwitchCase="'success'">Sucesso!</div>
+      <div *ngSwitchCase="'warning'">Atenção!</div>
+      <div *ngSwitchDefault></div>
+    </div>
   `
 })
 export class ItemComponent {
-
-	status: string;
-
+  status: string;
 }
