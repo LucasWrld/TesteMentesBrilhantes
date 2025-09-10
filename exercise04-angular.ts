8ec5fcd2ac9cd2d1f56ce1df047f76abab6efc30
LucasWrld <97101788+LucasWrld@users.noreply.github.com>
2025-09-10 15:18:38 -0300

    Update exercise04-angular.ts

--- exercise04-angular.ts
+++ exercise04-angular.ts
@@ -1,24 +1,33 @@
 /**
  * Evite vazamento de memória
  */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MyService } from './my.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-item',
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnDestroy {
 
-	items: string[] = [];
+  items: string[] = [];
+  private destroy$ = new Subject<void>();
 
-	constructor(private service: MyService) {}
+  constructor(private service: MyService) {}
 
-	ngOnInit() {
-		this.service
-			.getData()
-			.subscribe((data: any) => {
-				this.items = data;
-			});
-	}
+  ngOnInit() {
+    this.service
+      .getData()
+      .pipe(takeUntil(this.destroy$)) // garante o unsubscribe
+      .subscribe((data: any) => {
+        this.items = data;
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
 }
